1404059e4eff38d1c86b8c09beef59035967d6e5
Lee JaeHyung <leejh7830@naver.com>
2024-06-18 17:27:34 +0900

    20240618 17:30 test

---
+++ calculator_prac1.py
@@ -0,0 +1,28 @@
+# add subtract multiply divide
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by Zero."
+    return x / y
+
+
+def calculator():
+    print("원하는 계산의 번호를 입력하세요..")
+    print("1. 더하기(Add)")
+    print("2. 빼기(Sub)")
+    print("3. 곱하기(multi)")
+    print("4. 나누기(Div)")
+
+    while True:
+
